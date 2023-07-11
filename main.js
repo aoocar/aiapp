6a4f73d50ab55f078a2adfbad3ef3b98f44ecbab
飞 路 <aoobee@msn.com>
2023-07-12 00:33:22 +0800

    Initial commit

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
-// 获取 chat-container 元素
-const chatContainer = document.getElementById('chat-container');
+const apiUrl = 'https://api.dify.ai/v1/chat-messages';
+const conversationDiv = document.getElementById('conversation');
+const inputText = document.getElementById('inputText');
 
-// 定义调用 API 的函数
-async function callAPI() {
-  const apiUrl = 'https://api.dify.ai/v1/chat-messages';
-  const apiKey = 'app-zWoAxLPaBgKT8djav0EB6Gm9';
+async function sendMessage() {
+  const query = inputText.value.trim();
+  if (query === '') {
+    return;
+  }
 
   const requestBody = {
     inputs: {},
-    query: 'eh',
-    response_mode: 'streaming',
-    conversation_id: '1c7e55fb-1ba2-4e10-81b5-30addcea2276',
-    user: 'abc-123'
+    query: query,
+    response_mode: 'blocking',
+    conversation_id: '', // 如果要继续对话，请传入上下文返回的 conversation_id
+    user: 'abc-123' // 您的用户标识
   };
 
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${apiKey}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(requestBody)
-  });
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer app-zWoAxLPaBgKT8djav0EB6Gm9',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody)
+    });
 
-  const responseData = await response.json();
+    if (!response.ok) {
+      throw new Error('请求失败');
+    }
 
-  // 处理 API 响应的逻辑
-  // 在 chat-container 中显示回答
-  const answer = responseData.answer;
-  const answerElement = document.createElement('p');
-  answerElement.textContent = answer;
-  chatContainer.appendChild(answerElement);
+    const responseData = await response.json();
+    const answer = responseData.answer;
+    addMessageToConversation(query, answer);
+  } catch (error) {
+    console.error('发生错误:', error);
+  }
 }
 
-// 调用 API
-callAPI();
+function addMessageToConversation(query, answer) {
+  const message = document.createElement('p');
+  message.innerHTML = `<strong>你:</strong> ${query}<br><strong>AI:</strong> ${answer}`;
+  conversationDiv.appendChild(message);
+  inputText.value = '';
+}
