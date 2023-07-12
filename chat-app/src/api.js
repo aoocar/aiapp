ab0154502b616e05b7ac38c98da0223d5c88f6db
飞 路 <aoobee@msn.com>
2023-07-12 12:37:33 +0800

    Initial commit

--- chat-app/src/api.js
+++ chat-app/src/api.js
@@ -1,49 +1,23 @@
-// api.js
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.dify.ai/v1' 
+  baseURL: 'https://api.dify.ai/v1'
 })
 
 export default {
   getParams() {
-    return api.get('/parameters') 
+    return api.get('/parameters')
   },
 
   sendMessage(data) {
     return api.post('/chat-messages', data)
   },
 
-  // 其他接口方法
-}
-
-// 获取参数
-getParams() {
-    return api.get('/parameters')
-  }
-  
-  // 获取历史消息  
   getMessages(params) {
-    return api.get('/messages', {params}) 
-  }
-  
-  // 发送反馈
+    return api.get('/messages', { params })
+  },
+
   sendFeedback(id, data) {
     return api.post(`/messages/${id}/feedbacks`, data)
   }
-
-  // api.js 
-
-export default {
-
-    getParams() {
-      return axios.get('/parameters') 
-    },
-  
-    sendMessage(data) {
-      return axios.post('/chat-messages', data)
-    }
-  
-    // 其他接口同理
-  
-  }
+}
