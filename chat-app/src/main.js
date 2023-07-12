cc27d12f79df2bcdcec8ad9b47076c26e392e462
飞 路 <aoobee@msn.com>
2023-07-12 11:29:28 +0800

    Initial commit

--- chat-app/src/main.js
+++ chat-app/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+// 替换前
+// import App from './App.vue'
+
+//createApp(App).mount('#app')
+
+// 替换后
+import Chat from './components/Chat.vue' 
+
+createApp(Chat).mount('#app')
